a11aa07f3114a28e17007beb98a4ef2aa8899732
marcosware <141648729+marcosware@users.noreply.github.com>
2023-09-19 10:24:55 -0300

    External JS File
    Scripts now are in a external js file for better readability
    Removed unused CSS style

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function accordion(id){
+    if (id.style.display === "block") {
+        id.style.display = "none";
+    } else {
+        id.style.display = "block";
+    }
+}
+    function hide(id) {
+    id.style.display="none";
+}
+    function show(id) {
+    id.style.display="block";
+}
