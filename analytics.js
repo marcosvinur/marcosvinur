7346506b47e0ffa254a7cd869b1ca50536f9a701
marcosware <141648729+marcosware@users.noreply.github.com>
2023-11-30 10:08:44 -0300

    Analytics Code

--- analytics.js
+++ analytics.js
@@ -3,11 +3,11 @@ newScript.type = "text/javascript";
 newScript.setAttribute("async", "true");
 
 newScript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-
-XXXXXXXXXX");
+VGP215QVXJ");
 
 document.documentElement.firstChild.appendChild(newScript);
 // Executando a funcao gtag(), que esta disponivel no script acima
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-XXXXXXXXXX');
+gtag('config', 'G-VGP215QVXJ');
