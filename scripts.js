e4a056d66669797cfe503cc51c070f93a379c76f
marcosware <141648729+marcosware@users.noreply.github.com>
2024-05-08 16:49:21 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
-function accordion(id){
-    if (id.style.display === "block") {
-        id.style.display = "none";
-    } else {
-        id.style.display = "block";
-    }
-}
-    function hide(id) {
-    id.style.display="none";
-}
-    function show(id) {
-    id.style.display="block";
-}
+function darkMode() {
+    var bodyEl = document.body;
+    bodyEl.classList.toggle("dark-mode");
+    var borderEl = document.getElementById("teste");
+    borderEl.classList.toggle("dark-mode");
+    var navEl1 = document.getElementById("a1");
+    navEl1.classList.toggle("dark-mode");
+    var navEl2 = document.getElementById("a2");
+    navEl2.classList.toggle("dark-mode");
+    var navEl3 = document.getElementById("a3");
+    navEl3.classList.toggle("dark-mode");
+  }
